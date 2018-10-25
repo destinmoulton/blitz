7309309cd2161bef08ed97ca4964d55941e80056
Destin Moulton <destin.moulton@gmail.com>
2018-10-25 13:51:22 -0600

    Renamed scrapers meta.

---
+++ models/scrapersmeta.model.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * scrapers model to hold the meta info for the last time a scraper ran
+ */
+
+const Sequelize = require("sequelize");
+
+const model = {
+    table: "scrapers_meta",
+    attributes: {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        scraper: Sequelize.STRING,
+        last_scraped: Sequelize.DATE
+    }
+};
+
+class ScrapersMetaModel {
+    constructor(sequelize) {
+        this.sequelize = sequelize;
+    }
+
+    async initialize() {
+        this.ScrapersMeta = await this.sequelize
+            .define(model.table, model.attributes)
+            .sync();
+    }
+
+    async getAll() {
+        return await this.ScrapersMeta.findAll();
+    }
+}
+
+module.exports = ScrapersMetaModel;
